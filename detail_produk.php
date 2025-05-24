f6280a3253fcacfe2b15ce9d0fbe25b680ef1c55
dewi nuraeni <dewinuraeni057@gmail.com>
2025-05-24 19:54:37 +0700

    membenahi header part detail_produk

--- detail_produk.php
+++ detail_produk.php
@@ -121,38 +121,6 @@
   </header>
   <!-- Header part end-->
 
-  <div class="hearer_icon d-flex">
-    <a id="search_1" href="javascript:void(0)"></a>
-
-    <div class="dropdown cart">
-      <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true"
-        aria-expanded="false">
-        <i class="fas fa-cart-plus"></i>
-      </a>
-      <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <div class="single_product">
-
-                                </div>
-                            </div> -->
-
-    </div>
-  </div>
-  </nav>
-  </div>
-  </div>
-  </div>
-  <div class="search_input" id="search_input_box">
-    <div class="container ">
-      <form class="d-flex justify-content-between search-inner">
-        <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-        <button type="submit" class="btn"></button>
-        <span class="ti-close" id="close_search" title="Close Search"></span>
-      </form>
-    </div>
-  </div>
-  </header>
-  <!-- Header part end-->
-
   <!-- breadcrumb start-->
   <section class="breadcrumb breadcrumb_bg">
     <div class="container">
